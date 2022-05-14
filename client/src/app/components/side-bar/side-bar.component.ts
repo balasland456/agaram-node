15a94d7433bbc5ba95aca445d58806f4c1debf1f
Barath C <barath@capeagency.in>
2022-05-14 12:27:27 +0530

    created admin dasboard

---
+++ client/src/app/components/side-bar/side-bar.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.scss']
+})
+export class SideBarComponent implements OnInit {
+
+  @Input()
+  heading: string = ""
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
